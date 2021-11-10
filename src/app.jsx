515478282696be892a9e45a3c5bafa332c658f7e
pauvrelieu <pauvrelieu@gmail.com>
2021-11-10 08:51:56 +0100

    filter by region

--- src/app.jsx
+++ src/app.jsx
@@ -4,6 +4,8 @@ import { CountryItem } from './components/CountryItem'
 export function App() {
   const [countries, setCountries] = useState([])
   const [search, setSearch] = useState('')
+  const [regions, setRegions] = useState([])
+  const [region, setRegion] = useState('')
 
   useEffect(() => {
     let endpoint = 'all'
@@ -12,21 +14,44 @@ export function App() {
       endpoint = `name/${search}`
     }
 
+    if (region) {
+      endpoint = `region/${region}`
+    }
+
     fetch(`https://restcountries.com/v2/${endpoint}`)
       .then((response) => response.json())
       .then(setCountries)
-  }, [search])
+  }, [search, region])
 
-  const handleInput = function (e) {
+  useEffect(() => {
+    fetch('regions.json')
+      .then((response) => response.json())
+      .then(setRegions)
+  }, [])
+
+  const handleSearch = function (e) {
     setSearch(e.target.value)
+  }
+
+  const handleRegion = function (e) {
+    setRegion(e.target.value)
   }
 
   return (
     <>
-      <input type="text" onInput={handleInput} />
+      <input type="text" onInput={handleSearch} />
+
+      <select onChange={handleRegion}>
+        <option value="">Filter by Region</option>
+        {regions.map((region) => (
+          <option key={region.toLowerCase()} value={region.toLowerCase()}>
+            {region}
+          </option>
+        ))}
+      </select>
 
       {countries.map((country) => (
-        <CountryItem country={country} />
+        <CountryItem key={country.name} country={country} />
       ))}
     </>
   )
